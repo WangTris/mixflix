2707bda864163a999892f24235ac4b634ec18b2c
TriDev <tranwangtristudy@gmail.com>
2024-03-11 18:00:53 +0700

    updated style and app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// components
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./index";
+
+// styles
+import { Root, Content, Toolbar } from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Root>
+      <BrowserRouter>
+        <CssBaseline />
+        <NavBar />
+        <Content>
+          <Toolbar />
+          <Routes>
+            <Route exact path="/" element={<Movies />} />
+            <Route exact path="/movie/:id" element={<MovieInformation />} />
+            <Route exact path="/actors/:id" element={<Actors />} />
+            <Route exact path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </Content>
+      </BrowserRouter>
+    </Root>
+  );
 };
 
 export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,16 @@
+import { styled } from "@mui/system";
+
+// App.js //
+export const Root = styled("div")({
+  display: "flex",
+  height: "100%",
+});
+
+export const Toolbar = styled("div")({
+  width: "70px",
+});
+
+export const Content = styled("main")({
+  flexGrow: 1,
+  padding: "2em",
+});
